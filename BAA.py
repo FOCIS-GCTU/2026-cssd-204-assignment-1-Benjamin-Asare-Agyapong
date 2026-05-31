0e940df0f99534ad2e98e109dc0815f2adaf453e
be444 <benjaminasareagyapong2006@gmail.com>
2026-05-31 12:48:53 +0000

    Add BAA.py

---
+++ BAA.py
@@ -0,0 +1,35 @@
+# File: BAA.py
+# Description: Print out my initials in stylized large block letters.
+# Assignment Number: 1
+#
+# Name: Benjamin Asare Agyapong
+# STUDENT ID: 2425404015
+# Email: benjaminasareagyapong2006@gmail.com
+# Grader: Augustus Buckman
+#
+# On my honor, Benjamin Asare Agyapong, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
+
+def main():
+    # Print initials BAA as large 12x10 block letters made of B and A characters and periods,
+    # each followed by a 5-wide period slot (..** on rows 8-9), separated by 3-dot columns.
+    # Total output is 10 rows high and 60 characters wide, with no trailing spaces.
+
+    print()
+    print("...BAA")
+    print()
+    print("...BBBBBBB.................AAAA................AAAA.........")
+    print("...BB.....BB..............AA..AA..............AA..AA........")
+    print("...BB.....BB.............AA....AA............AA....AA.......")
+    print("...BB.....BB............AA......AA..........AA......AA......")
+    print("...BBBBBBB.............AAAAAAAAAAAA........AAAAAAAAAAAA.....")
+    print("...BB.....BB...........AA........AA........AA........AA.....")
+    print("...BB......BB..........AA........AA........AA........AA.....")
+    print("...BB......BB..........AA........AA........AA........AA.....")
+    print("...BB.....BB......**...AA........AA...**...AA........AA...**")
+    print("...BBBBBBB........**...AA........AA...**...AA........AA...**")
+    print()
+
+
+main()
